03300c6ba169144c60c5d15c569556d4f04473cf
Timothy Haley <tim.d.haley@gmail.com>
2020-10-07 16:07:54 -0400

    WIP

---
+++ server/src/api/temp.js
@@ -0,0 +1,10 @@
+// const request = require('supertest');
+// const api = require('./index');
+
+// describe('API', () => {
+//   it('pings successfully', () => (
+//     request(api)
+//       .get('/ping')
+//       .expect(200)
+//   ));
+// });
